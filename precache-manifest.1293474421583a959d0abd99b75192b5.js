09d1730253790a90f8ee4713ecfee5b3b52528ee
David Mihal <david@dmihal.com>
2020-10-19 20:08:04 +0100

    Updates

---
+++ precache-manifest.1293474421583a959d0abd99b75192b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf3201bf47920a47f71a8600ce812e9",
+    "url": "/pay-with-dai/index.html"
+  },
+  {
+    "revision": "e647ec3537f6374f779a",
+    "url": "/pay-with-dai/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76e2a5a18300e40e9225",
+    "url": "/pay-with-dai/static/js/2.2a99ea03.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pay-with-dai/static/js/2.2a99ea03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e647ec3537f6374f779a",
+    "url": "/pay-with-dai/static/js/main.b7d0f3d8.chunk.js"
+  },
+  {
+    "revision": "91091faaec9ce1bac566",
+    "url": "/pay-with-dai/static/js/runtime-main.d571b4fe.js"
+  }
+]);
